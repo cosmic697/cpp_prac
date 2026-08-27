35223464d74bb0c2dafaf9dbcc1b2ab0c4198102
697-rgb <pushkargarg697@gmail.com>
2026-08-27 19:31:35 +0530

    commit 1

---
+++ circumference.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2* PI * radius ;
+    std::cout << "the circumference is "<<circumference;
+    return 0;
+}
